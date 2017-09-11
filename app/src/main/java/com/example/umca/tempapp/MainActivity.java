71cb00b36a77e09c9e0fb718411dd66d3dd30f96
Sergey <battleumca@mail.ru>
2017-09-11 22:15:26 +0300

    stage2

--- app/src/main/java/com/example/umca/tempapp/MainActivity.java
+++ app/src/main/java/com/example/umca/tempapp/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.umca.tempapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.view.*;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import com.example.umca.tempapp.barcode.BarcodeCaptureActivity;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 
@@ -14,29 +24,38 @@ import com.google.zxing.WriterException;
 
 public class MainActivity extends Activity {
 
-    ShowBarCode SHBC=new ShowBarCode();
-    Bitmap bitmap=null;
-    SharedPreferences sPref;
-    String CardCode="BonusNumber";
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int BARCODE_READER_REQUEST_CODE = 1;
+    private ShowBarCode SHBC = new ShowBarCode();
+    private Bitmap bitmap = null;
+    private SharedPreferences sPref;
+    private String CardCode = "BonusNumber";
+    private EditText tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      // requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
-
-
+        Button scanBarcodeButton = findViewById(R.id.scan_barcode_button);
+        scanBarcodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
+                startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
+            }
+        });
 
 
         // barcode image
 
-        final ImageView iv=(ImageView) findViewById(R.id.imageView2);
-        final EditText tv =(EditText) findViewById(R.id.editText);
+        final ImageView iv = findViewById(R.id.imageView2);
+        tv = findViewById(R.id.editText);
         String barcode_data=loadPref(); // загружаем номер карты из файла
 
 
@@ -44,8 +63,7 @@ public class MainActivity extends Activity {
             @Override
             public boolean onKey(View view, int i, KeyEvent keyEvent) {
                 if(keyEvent.getAction() == KeyEvent.ACTION_DOWN &&
-                        (i == KeyEvent.KEYCODE_ENTER))
-                {
+                        (i == KeyEvent.KEYCODE_ENTER)) {
                     // сохраняем текст, введенный до нажатия Enter в переменную
                     String strCatName = tv.getText().toString();
                     try {
@@ -54,12 +72,10 @@ public class MainActivity extends Activity {
                         iv.setImageBitmap(bitmap);// отображаим штрихкод
 
 
-
                     } catch (WriterException e) {
                         e.printStackTrace();
                     }
                     savePerf(strCatName);
-
 
 
                     return true;
@@ -67,8 +83,6 @@ public class MainActivity extends Activity {
                 return false;
             }
         });
-
-
 
 
         try {
@@ -81,37 +95,47 @@ public class MainActivity extends Activity {
         }
 
 
-
         //barcode text
 
         tv.setGravity(Gravity.CENTER_HORIZONTAL);
         tv.setText(barcode_data);
 
 
-
     }
 
 
-    String loadPref()
-    {
+    String loadPref() {
         sPref=getSharedPreferences("Test2",MODE_PRIVATE); // модификатор
         String barcode_data = sPref.getString(CardCode,""); // получить поле, по значению
         return barcode_data;
     }
 
 
-    void savePerf(String strCatName)
-    {
+    void savePerf(String strCatName) {
         sPref=getSharedPreferences("Test2",MODE_PRIVATE); // модификатор-Константа MODE_PRIVATE используется для настройки доступа и означает, что после сохранения, данные будут видны только этому приложению.
         SharedPreferences.Editor editor=sPref.edit(); //чтобы редактировать данные, необходим объект Editor – получаем его из sPref
         editor.putString(CardCode,strCatName); // Изменить пол и значение
         editor.commit(); // записать
     }
 
-
-
-
-
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BARCODE_READER_REQUEST_CODE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Point[] p = barcode.cornerPoints;
+                    tv.setText(barcode.displayValue);
+                } else tv.setText(R.string.no_barcode_captured);
+            } else Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
+                    CommonStatusCodes.getStatusCodeString(resultCode)));
+        } else super.onActivityResult(requestCode, resultCode, data);
+    }
 }
+
+
+
+
+
+
+
 
